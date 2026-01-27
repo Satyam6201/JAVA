7f9a31f1d15622a79589b142d33e06a7b3fbe4fe
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-01-27 13:18:40 +0530

    first commit

--- ArrayAll.java
+++ ArrayAll.java
@@ -1,4 +1,3 @@
-
 import java.util.HashMap;
 
 public class ArrayAll {
@@ -134,6 +133,162 @@ public class ArrayAll {
         return new int[]{-1, -1};
     }
     
+    // 9. First Occurence(Jo pahli baar number aaega)
+    public static void firstOccurence(int arr[], int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                high = mid - 1;
+            }
+
+            else if (arr[mid] < target) {
+                low = mid + 1;
+            }
+
+            else {
+                high = mid - 1;
+            }
+        }
+        System.out.println("Index of first Occurence = " + ans);
+    }
+    
+    // 10. Last Occurence(Jo last me aata hai)
+    public static void lastOccurence(int arr[], int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else if (arr[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        System.out.println("Index of last Occurence = " + ans);
+    }
+   
+    // 11. Search in rotated sorted array
+    public static int search(int arr[], int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            // Left side sorted
+            if (arr[low] <= arr[mid]) {
+                if (target >= arr[low] && target < arr[mid]) {
+                  high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+
+            // right side sorted
+            else {
+                if (target > arr[mid] && target <= arr[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    // 12. Find peak Number 
+    public static void peakNumber(int arr[]) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid;
+            }
+        }
+        System.out.println("index = " + low + " Value = " + arr[low]);
+    } 
+
+    // 13. Merge Two sorted Array
+    public static int[] mergeArray(int arr1[], int arr2[]) {
+        int n = arr1.length;
+        int m = arr2.length;
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        
+        int result[] = new int[n + m]; 
+        
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                result[k] = arr1[i];
+                k++;
+                i++;
+            }
+            else {
+                result[k] = arr2[j];
+                k++;
+                j++;
+            }
+        }
+
+        while (i < n) {
+            result[k] = arr1[i];
+            k++;
+            i++;
+        }
+
+        while (j < m) {
+            result[k] = arr2[j];
+            k++;
+            j++;
+        }
+
+        return result;
+    }
+
+    // 14.buy and shell stock
+    public static void buyShell(int prices[]) {
+        int minPrices = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - minPrices;
+
+            maxProfit = Math.max(maxProfit, profit);
+            minPrices = Math.min(minPrices, prices[i]);
+        }
+
+        System.out.println(maxProfit);
+        System.out.println("Index where we have profit = " + prices[maxProfit]);
+    }
+
     // 2. Find duplicates in an array
     public static void findDuplicates(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
@@ -175,8 +330,8 @@ public class ArrayAll {
         // int arr[] = {2, 3, 5, 4, 5, 3, 2};
         // findSingle(arr);
     
-        int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        maxSubArray(arr);
+        // int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        // maxSubArray(arr);
 
         // int[] arr = {2, 7, 11, 15};
         // int target = 9;
@@ -184,5 +339,28 @@ public class ArrayAll {
 
         // int [] result = twoSumByHashMap(arr, target);
         // System.out.println(result[0] + " " + result[1]);
+    
+        // int arr[] = {1, 2, 2, 2, 3, 4};
+        // int target = 2;
+        // firstOccurence(arr, target);
+        // lastOccurence(arr, target);
+
+        // int arr[] = {4, 5, 6, 7, 0, 1, 2};
+        // int Target = 0;
+        // System.out.println(search(arr, Target));
+
+        // int arr[] = {1, 2, 1, 3, 5, 6, 4};
+        // peakNumber(arr);
+
+        // int arr1[] = {1, 4, 6};
+        // int arr2[] = {2, 3, 5, 8, 9};
+        // int result[] = mergeArray(arr1, arr2);
+        // for (int i = 0; i < result.length; i++) {
+        //     System.out.print(result[i] + " ");
+        // }
+
+
+        int prices[] = {7, 1, 5, 3, 6, 4};
+        buyShell(prices);
     }
 }
