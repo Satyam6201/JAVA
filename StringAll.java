4b4f50d09afef0754f74bc823977e2c46f5e7f51
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-02-05 11:59:52 +0530

    Final Commit

--- StringAll.java
+++ StringAll.java
@@ -18,10 +18,33 @@ public class StringAll{
 
         String reversed = new String(arr);
         System.out.println(reversed);
-
     }
+
+    // 2. CheckPalindrome 
+    public static boolean isPalindrome(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+
     public static void main(String[] args) {
-        String s = "hello";
-        reverseString(s);
+        // String s = "hello";
+        // reverseString(s); //olleh
+
+        String s = "madam";
+        if (isPalindrome(s)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
     }
 }
