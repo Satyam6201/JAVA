e254e835de975e6bd8d18621b09f5c4287b2cf66
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-04-03 09:29:31 +0530

    question added

--- stacks.java
+++ stacks.java
@@ -1,8 +1,9 @@
 import java.util.Stack;
 
 public class stacks {
+
     // Valid paranthesis
-    public static void validParentheses(String str) {
+    public static boolean validParentheses(String str) {
         Stack<Character> s = new Stack<>();
 
         for (int i = 0; i < str.length(); i++) {
@@ -13,8 +14,7 @@ public class stacks {
             }
             else {
                 if (s.isEmpty()) {
-                    System.out.println("Not a Valid Parantheses!");
-                    return;
+                    return false;
                 }
                 else if ((s.peek() == '(' && ch == ')') || (s.peek() == '{' && ch == '}') || (s.peek() == '[' && ch == ']')) {
                     s.pop();
@@ -22,14 +22,70 @@ public class stacks {
             }
         }
         if (s.isEmpty()) {
-            System.out.println("Valid Paranthese!");
+            return true;
         }
-        else {
-            System.out.println("Not a Valid Parantheses!");
+        return false;
+    }
+    
+    // Reverse string using stack
+    public static void reverseString(String str) {
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            s.push(str.charAt(i));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!s.isEmpty()) {
+            sb.append(s.pop());
+        }
+
+        System.out.println(sb);
+    }
+    
+    // Next greater element
+    public static void nextGreater(int arr[]) {
+        Stack<Integer> s = new Stack<>();
+        int temp[] = new int[arr.length];
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            // jab tak stack ka top current element se chhota ya equal hai
+            // tab tak usko hata do (kyunki wo next greater nahi ho sakta)
+           while (!s.isEmpty() && s.peek() <= arr[i]) {
+            s.pop();
+           }
+
+            // agar stack empty ho gaya  koi bada element nahi mila
+           if (s.isEmpty()) {
+            temp[i] = -1;
+           }
+           // warna stack ka top hi next greater element hai
+           else {
+            temp[i] = s.peek();
+           }
+
+            // ab current element ko stack me daal do
+            // taaki future elements ke liye use ho sake
+            s.push(arr[i]);
+        }
+
+        for (int i = 0; i < temp.length; i++) {
+            System.out.print(temp[i] + " ");
         }
     }
+    
+    // Previous smallest element
+    
+    
+    
     public static void main(String[] args) {
-        String str = "({})";
-        validParentheses(str);
+        // String str = "({})";
+        // System.out.println(validParentheses(str));
+
+        // String str = "hello";
+        // reverseString(str);
+
+        int arr[] = {6, 8, 0, 1, 3};
+        nextGreater(arr);
     }
 }
