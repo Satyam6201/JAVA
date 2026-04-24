dcb251b75a23c3aaaae2d34307fb5fe9ca20bbe7
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-04-24 12:47:43 +0530

    adding more Questions

--- Trees.java
+++ Trees.java
@@ -314,16 +314,6 @@ public class Trees {
         return max;
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     public static void main(String[] args) {
         // int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         // BinaryTree tree = new BinaryTree();
