9432f62f48d22297d05920e3b97ab54122550b44
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-05-24 22:35:13 +0530

    adding more Questions

--- greedyAlgorithm.java
+++ greedyAlgorithm.java
@@ -168,7 +168,6 @@ public class greedyAlgorithm {
         // Q6
         int[] start = {1, 3, 0, 5, 8, 5};
         int[] end   = {2, 4, 6, 7, 9, 9};
-
         System.out.println(maxMeeting(start, end));
     }
 }
