233bdf5177eee24d19248960c56f0934ffe32799
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-05-29 12:30:15 +0530

    adding more Questions

--- dp.java
+++ dp.java
@@ -44,14 +44,48 @@ public class dp {
         return prev;
     }
     
+    // Q3. House Robber
+    public static int rob(int arr[]) {
+        int n = arr.length;
+
+        if (n == 0) return 0;
+        if (n == 1) return arr[0];
+
+        int dp[] = new int[n];
+
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + arr[i]);
+        }
+        return dp[n - 1];
+    }
     
+    // Q4. Unique Paths
+    public static int uniquePath(int m, int n) {
+        long res = 1;
+
+        for (int i = 1; i <= m - 1; i++) {
+            res = res * (n - 1 + i) / i;
+        }
+        return (int)res;
+    }
     public static void main(String[] args) {
         // Q1
         // int n = 4;
-        // stair(n);
+        // stair(n);  // 5
 
         // Q2
-        int arr[] = {30, 10, 60, 10, 60, 50};
-        System.out.println(frogJump(arr));
+        // int arr[] = {30, 10, 60, 10, 60, 50};
+        // System.out.println(frogJump(arr));  // 40
+
+        // Q3. 
+        // int arr[] = {1, 2, 3, 1};
+        // System.out.println(rob(arr));
+
+        // Q4.
+        System.out.println(uniquePath(3, 2));
+
     }
 }
