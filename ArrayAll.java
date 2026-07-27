6ba21ba55e0f3efda6192ac6e2a003537f61afe9
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-07-27 13:25:27 +0530

    Adding Que

--- ArrayAll.java
+++ ArrayAll.java
@@ -1,5 +1,5 @@
-import java.lang.String;
-import java.util.*;
+    import java.lang.String;
+    import java.util.*;
 
 public class ArrayAll {
     // 1. Second largest number 
@@ -1219,8 +1219,44 @@ public class ArrayAll {
             reverse(arr, 0, n - 1);
         }
         return arr;
-        }
+    }
     
+    // 58 3Sum
+    public static void ThreeSum(int arr[]) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int sum = 0;
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+
+            if (i >= 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = arr.length - 1;
+
+            while (left < right) {
+                sum = arr[i] + arr[left] + arr[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(arr[i], arr[left], arr[right]));
+
+                    while (left < right && arr[left] == arr[left + 1]) {
+                        left++;
+                    }
+
+                    while (left < right && arr[right] == arr[right - 1]) {
+                        right--;
+                    }
+                }
+                left++;
+                right--;
+            }
+        }
+        System.out.println(result);
+    }
     
 
 
@@ -1424,20 +1460,19 @@ public class ArrayAll {
         // rotate(matrix);
     
     
-        int arr[] = {1, 2, 3, 4, 5};
+        // int arr[] = {1, 2, 3, 4, 5};
         // leftRotate(arr); //2 3 4 5 1
         // rotateLeftArray(arr, 2); // 3 4 5 1 2 
         // System.out.println("");
         // rotateRightArray(arr, 2); //4 5 1 2 3 
-
-        String direction = "right";
-        rotateArray(arr, 2, direction);
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");//4 5 1 2 3  
-        }
+        // String direction = "right";
+        // rotateArray(arr, 2, direction);
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.print(arr[i] + " ");//4 5 1 2 3  
+        // }
        
-    
+        int arr[] = {-1,0,1,2,-1,-4};
+        ThreeSum(arr);
     
     }
 }
